9f75fea76100bd7b4057db8e27534d32fdb9b975
oscar-mx <oscarmx0912@outlook.com>
2020-09-18 16:07:56 +0800

    ctype-php

---
+++ PHP/ctype.php
@@ -0,0 +1,13 @@
+<?php
+// 字符类型检测函数 ctype 返回值 bool
+//ctype_alnum — 做字母和数字字符检测
+//ctype_alpha — 做纯字符检测
+//ctype_cntrl — 做控制字符检测
+//ctype_digit — 做纯数字检测
+//ctype_graph — 做可打印字符串检测，空格除外
+//ctype_lower — 做小写字符检测
+//ctype_print — 做可打印字符检测
+//ctype_punct — 检测可打印的字符是不是不包含空白、数字和字母
+//ctype_space — 做空白字符检测
+//ctype_upper — 做大写字母检测
+//ctype_xdigit — 检测字符串是否只包含十六进制字符
